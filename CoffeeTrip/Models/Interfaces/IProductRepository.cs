7c489b4385def178bb8ed17ed8242a203739a56f
sid-rahman <siddik.iitju@gmail.com>
2024-11-23 01:34:49 +0600

    Created model and repository

---
+++ CoffeeTrip/Models/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeTrip.Models.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        IEnumerable<Product> GetAllTrendingProducts();
+        Product GetProductDetailsById(int id);
+    }
+}
