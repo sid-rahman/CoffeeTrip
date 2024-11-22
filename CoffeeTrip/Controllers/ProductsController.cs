2b4a5740468344ef5feeb4465292a25ccbf5b321
sid-rahman <siddik.iitju@gmail.com>
2024-11-23 02:09:50 +0600

    Created controller and view

---
+++ CoffeeTrip/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using CoffeeTrip.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeTrip.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository _productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+                _productRepository = productRepository;
+        }
+
+        public IActionResult Shop()
+        {
+            return View(_productRepository.GetAllProducts());
+        }
+    }
+}
--- CoffeeTrip/Program.cs
+++ CoffeeTrip/Program.cs
@@ -1,7 +1,11 @@
+using CoffeeTrip.Models.Interfaces;
+using CoffeeTrip.Models.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
