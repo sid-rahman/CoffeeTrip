239fe424d6f5f0efc89f51620f2431a084328206
sid-rahman <siddik.iitju@gmail.com>
2024-12-09 01:23:11 +0600

    Added controller and views for ordering process

---
+++ CoffeeTrip/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using CoffeeTrip.Models;
+using CoffeeTrip.Models.Interfaces;
+using CoffeeTrip.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeTrip.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private IOrderRepository orderRepository;
+        private IShoppingCartRepository shoppingCartRepository;
+        
+        public OrdersController (IOrderRepository orderRepository, IShoppingCartRepository shoppingCartRepository)
+        {
+            this.orderRepository = orderRepository;
+            this.shoppingCartRepository = shoppingCartRepository;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            orderRepository.PlaceOrder(order);
+            shoppingCartRepository.ClearCart();
+            HttpContext.Session.SetInt32("CartCount", 0);
+            return RedirectToAction("CheckoutComplete");
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            return View();
+        }
+
+
+    }
+}
